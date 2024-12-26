e56b50c3479f961e3c61318750d8c767760267c3
v.vardanyan <vako3008@gmail.com>
2024-12-26 16:09:59 +0400

    improve dimensions

--- src/components/Lives.jsx
+++ src/components/Lives.jsx
@@ -4,7 +4,7 @@ const Lives = () => {
     const {lives} = useAppContext();
     return (
         <div style={{position:"absolute", right:0, fontSize:40, zIndex: 10, color:"white", background:"blue"}}>{lives}</div>
-    )
-}
+    );
+};
 
 export default Lives;
--- src/components/Score.jsx
+++ src/components/Score.jsx
@@ -4,7 +4,7 @@ const Score = () => {
     const {score} = useAppContext();
     return (
         <div style={{position:"absolute", fontSize:40, zIndex: 10, color:"white", background:"blue"}}>{score}</div>
-    )
-}
+    );
+};
 
 export default Score;
