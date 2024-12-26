bfd7f45efb1994950f55f02941d991e11156eccc
v.vardanyan <vako3008@gmail.com>
2024-12-26 15:36:41 +0400

    implement speed up

--- src/components/Snowfall/Snowfall.jsx
+++ src/components/Snowfall/Snowfall.jsx
@@ -1,4 +1,4 @@
-import React, {useCallback, useEffect, useRef, useState} from 'react';
+import React, {useEffect, useRef} from 'react';
 import {useAppContext} from "../../Context";
 
 import snowImg from "./snow.png";
